ae651b45f30d90f68cfd2cabe8d59c4204052ee0
Fireball <61852375+Fireball734@users.noreply.github.com>
2025-01-21 17:00:15 -0500

    edited the code so it grows instead of shrinks

--- MovingAround.py
+++ MovingAround.py
@@ -9,7 +9,7 @@ class MovingAround(Scene):
         self.play(square.animate.shift(LEFT))
         #turns the quare orange
         self.play(square.animate.set_fill(ORANGE))
-        #shrink the square to 3/10 of its size
-        self.play(square.animate.scale(0.3))
-        #Rotate the square clockwise at a rate of 0.4
-        self.play(square.animate.rotate(0.4))
+        #grow the square to 3 times its size
+        self.play(square.animate.scale(3))
+        #Rotate the square clockwise at a rate of 4
+        self.play(square.animate.rotate(4))
